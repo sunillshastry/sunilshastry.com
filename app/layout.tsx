f35bd832a10adcc69812649ab620bbf410d13d45
sunil <sunil.shastry69@gmail.com>
2025-09-13 02:34:02 -0600

    Change and configure metadata object within layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,48 @@ const inter = Inter({
 
 export const metadata: Metadata = {
 	title: 'Sunil Shastry | University of Saskatchewan',
+	applicationName: 'sunilshastry.com',
+	description:
+		'Sunil Shastry is an undergraduate student at the University of Saskatchewan majoring in Computer Science and minoring in Theoretical Mathematics. His primary expertise lies in Human-Computer Interaction and Web Development',
+	keywords: [
+		'Sunil Shastry',
+		'Sunil',
+		'Shastry',
+		'Sunil Shastri',
+		'University of Saskatchewan',
+	],
+	authors: [
+		{
+			name: 'Sunil Shastry',
+		},
+	],
+	creator: 'Sunil Shastry',
+	publisher: 'Sunil Shastry',
+
+	openGraph: {
+		title: 'Sunil Shastry',
+		description:
+			'Sunil Shastry is an undergraduate student at the University of Saskatchewan majoring in Computer Science and minoring in Theoretical Mathematics. His primary expertise lies in Human-Computer Interaction and Web Development',
+		url: 'https://sunilshastry.com',
+		siteName: 'sunilshastry.com',
+		images: [
+			{
+				url: 'https://sunilshastry.com/me.jpg',
+				alt: `Sunil Shastry's Photo`,
+			},
+		],
+		locale: 'en_US',
+		type: 'website',
+	},
+
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+		},
+	},
 };
 
 export default function RootLayout({
