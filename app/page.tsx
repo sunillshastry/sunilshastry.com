28e4846929b5159a7403d4bb7260cf93ecb433f2
sunil <sunil.shastry69@gmail.com>
2025-09-12 23:34:42 -0600

    Add website content to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
 export default function Page() {
 	return (
-		<div>
-			<h1>Hello world!</h1>
-		</div>
+		<section>
+			<div>
+				<h2>Namaste,</h2>
+				<h1>My name is Sunil Shastry.</h1>
+			</div>
+
+			<div>
+				<p>
+					I am an undergraduate student at the University of Saskatchewan ,
+					Canada, currently pursuing a Bachelor of Science degree with a major
+					in Computer Science and a minor in Theoretical Mathematics. My
+					academic interests within computer science primarily focus on Web
+					Development and Human-Computer Interaction (HCI) , where I explore the
+					design, functionality, and user experience aspects of digital systems.
+					In the field of mathematics, I am particularly drawn to Linear Algebra
+					and Discrete Mathematics , as they complement and provide foundational
+					insights into abstract structures, logical reasoning, and
+					computational applications. View resume →
+				</p>
+
+				<p>
+					Outside of programming and university, I enjoy the activities of
+					cooking, hiking, and watching movies (mostly thriller). Feel free to
+					connect with me on LinkedIn or view my open-source work and projects
+					on GitHub.
+				</p>
+			</div>
+		</section>
 	);
 }
