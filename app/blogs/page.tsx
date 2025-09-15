834d8d9348c8cf2966e03b1867f0397c91e6bb0d
sunil <sunil.shastry69@gmail.com>
2025-09-15 09:31:26 -0600

    Develop the index /blogs page route with page.tsx: Setup all blogs as a list with links and good UI convention(s)

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,9 +1,62 @@
-import BlogHeader from '@/components/blog-header';
+import { getAllPosts } from '@/lib/mdx';
+import getFormattedDate from '@/utils/get-formatted-date';
+import getFormattedMonth from '@/utils/get-formatted-month';
+import getYear from '@/utils/get-year';
+import nameShorthand from '@/utils/name-shorthand';
+import Link from 'next/link';
 
 export default function Page() {
+	const unsortedBlogs = getAllPosts();
+	const blogs = unsortedBlogs.sort(
+		(a, b) =>
+			new Date(b?.date as string).getTime() -
+			new Date(a?.date as string).getTime()
+	);
 	return (
-		<section className="mx-auto w-1/2 py-8 max-lg:w-[90%] max-lg:bg-red-500">
-			<BlogHeader />
-		</section>
+		<>
+			<div className="mt-2">
+				{blogs.map(function (blog) {
+					const year = getYear(blog.date as string);
+					const date = getFormattedDate(blog.date as string);
+					const month = getFormattedMonth(blog.date as string);
+
+					const monthShorthand = nameShorthand(month);
+
+					return (
+						<Link
+							href={`/blogs/${blog?.slug}`}
+							key={blog?.slug}
+							title={blog?.title}
+						>
+							<div className="ease flex w-full items-center justify-between rounded-md p-4 transition hover:bg-gray-400/25 max-lg:flex-col max-lg:items-start">
+								<div className="flex items-center max-lg:flex-col max-lg:items-start">
+									<h4 className="font-mono text-xs font-medium">
+										<span className="max-lg:hidden lg:inline-block">
+											{monthShorthand}
+										</span>{' '}
+										<span className="hidden max-lg:inline-block">{month}</span>{' '}
+										{date}, {year}
+									</h4>
+									<div className="ml-4 max-lg:mt-2 max-lg:ml-0">
+										<h4 className="text-sm font-semibold">{blog?.title}</h4>
+										<p className="mt-1 inline-block font-mono text-xs italic">
+											{(blog?.subtitle?.length as number) > 54
+												? `${blog?.subtitle?.slice(0, 55)}...`
+												: blog?.subtitle}
+										</p>
+									</div>
+								</div>
+
+								<div>
+									<h4 className="font-mono text-xs max-lg:mt-4">
+										{blog?.duration}
+									</h4>
+								</div>
+							</div>
+						</Link>
+					);
+				})}
+			</div>
+		</>
 	);
 }
