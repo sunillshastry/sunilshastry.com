96c8c2e124a4523f9a4ec001c59f9990dc029e9c
sunil <sunil.shastry69@gmail.com>
2025-09-15 09:40:25 -0600

    Setup blog post page with simple header section and render Markdown with MDXRemote component within page

---
+++ app/blogs/[slug]/page.tsx
@@ -0,0 +1,59 @@
+import { getPostBySlug } from '@/lib/mdx';
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
+import { MDXRemote } from 'next-mdx-remote-client/rsc';
+import mdxRemoteComponents from '@/mdx-components';
+import BlogTitle from '@/components/blog-title';
+import { BlogMeta } from '@/interfaces/BlogMeta';
+import { Metadata } from 'next';
+
+interface FunctionProps {
+	params: Promise<{ slug: string }>;
+}
+
+export async function generateMetadata({
+	params,
+}: FunctionProps): Promise<Metadata> {
+	const { slug } = await params;
+
+	const { data } = getPostBySlug(slug);
+
+	const title = `${data?.title} | Sunil Shastry`;
+
+	return {
+		title,
+		description: data?.description,
+	};
+}
+
+export default async function Page({ params }: FunctionProps) {
+	const { slug } = await params;
+
+	const { content, data } = getPostBySlug(slug);
+
+	return (
+		<section>
+			<Link
+				href="/blogs"
+				title="Blogs"
+				className="group inline-flex items-center justify-start gap-x-1 rounded-full bg-gray-400/25 px-4 py-2 text-sm font-medium"
+			>
+				<span className="transition group-hover:-translate-x-1">
+					<ArrowLeft size={12} />
+				</span>
+				<span>Blogs</span>
+			</Link>
+
+			<article className="mt-10">
+				<BlogTitle blog={data as BlogMeta} />
+
+				<div className="mt-8">
+					<MDXRemote
+						components={mdxRemoteComponents}
+						source={content as string}
+					></MDXRemote>
+				</div>
+			</article>
+		</section>
+	);
+}
