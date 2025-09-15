fb7cdd5e58470df0c31d25b677530d7528d63d13
sunil <sunil.shastry69@gmail.com>
2025-09-14 18:33:34 -0600

    Create a UI helper: theme-switch, to switch between themes

---
+++ components/theme-switch.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useTheme } from '@/context/ThemeContext';
+import { Moon, SunMedium } from 'lucide-react';
+
+const ICON_SIZE = 18;
+
+export default function ThemeSwitch() {
+	const { theme, lightSwitch, darkSwitch } = useTheme();
+
+	const lightStyles = `bg-white border-black/15 text-black`;
+	const darkStyles = `bg-black border-white/15 shadow-white/10 text-white`;
+
+	return (
+		<div
+			className={`fixed top-4 right-5 z-50 flex items-center justify-center gap-x-3 rounded-full border px-2 py-1.5 shadow-md ${theme === 'dark' ? darkStyles : lightStyles}`}
+		>
+			<button className="hover:cursor-pointer" onClick={lightSwitch}>
+				<SunMedium size={ICON_SIZE} />
+			</button>
+			<button
+				className={`${theme === 'light' ? 'border-black/15' : 'border-white/25'} border-l pl-3 hover:cursor-pointer`}
+				onClick={darkSwitch}
+			>
+				<Moon size={ICON_SIZE} />
+			</button>
+		</div>
+	);
+}
