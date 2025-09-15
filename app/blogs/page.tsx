9afb04d6fb824faa1f0ba7af7dd7c721db01a088
sunil <sunil.shastry69@gmail.com>
2025-09-15 05:01:03 -0600

    Setup an index page for /blogs route: currently only displaying blog-header component

---
+++ app/blogs/page.tsx
@@ -0,0 +1,9 @@
+import BlogHeader from '@/components/blog-header';
+
+export default function Page() {
+	return (
+		<section className="mx-auto w-1/2 py-8 max-lg:w-[90%] max-lg:bg-red-500">
+			<BlogHeader />
+		</section>
+	);
+}
