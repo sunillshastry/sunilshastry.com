b3347b966c6ede6155d46af4ffd7abc0edbdae58
sunil <sunil.shastry69@gmail.com>
2025-09-15 09:30:06 -0600

    Build BlogHeader component within blog-header.tsx source file: a simple nav-like header section for /blogs route

--- components/blog-header.tsx
+++ components/blog-header.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useTheme } from '@/context/ThemeContext';
-import { Github, Twitter } from 'lucide-react';
+import { ChevronRight, Github, Twitter } from 'lucide-react';
 import Link from 'next/link';
 
 export default function BlogHeader() {
@@ -9,17 +9,26 @@ export default function BlogHeader() {
 		<div
 			className={`flex items-center justify-between border-b px-3 py-2 pb-4 ${theme === 'dark' ? 'border-b-white/25' : 'border-b-black/15'}`}
 		>
-			<h3 className="font-black">Sunil Shastry</h3>
+			<h3 className="font-black">
+				<Link href="/" title="Sunil Shastry">
+					Sunil Shastry
+				</Link>
+			</h3>
 
 			<div className="flex items-center gap-x-2">
-				<span className="font-mono text-xs uppercase opacity-80">
-					Follow me
+				<span className="flex items-center gap-x-1 font-mono text-xs uppercase opacity-80">
+					<span>Follow me</span>
+					<span>
+						<ChevronRight size={12} />
+					</span>
 				</span>
 
 				<Link
 					href="https://x.com/sunillshastry"
 					target="_blank"
 					className="rounded-full p-2 transition hover:bg-gray-400/25"
+					aria-label="Twitter Icon"
+					title="Sunil's Twitter"
 				>
 					<Twitter />
 				</Link>
@@ -28,6 +37,8 @@ export default function BlogHeader() {
 					href="https://github.com/sunillshastry/"
 					target="_blank"
 					className="rounded-full p-2 transition hover:bg-gray-400/25"
+					aria-label="Github Icon"
+					title="Sunil's GitHub"
 				>
 					<Github />
 				</Link>
