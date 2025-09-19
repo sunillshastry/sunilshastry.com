61610b36827930e7c627f59a18e5ecf2ec0148a0
sunil <sunil.shastry69@gmail.com>
2025-09-19 02:50:20 -0600

    Update share-tab component: add new option to copy blog post URL with micro-interaction

--- components/share-tab.tsx
+++ components/share-tab.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { BlogMeta } from '@/interfaces/BlogMeta';
-import { ChevronDown } from 'lucide-react';
+import { Check, ChevronDown, Link } from 'lucide-react';
 import {
 	FacebookShareButton,
 	FacebookIcon,
@@ -9,12 +9,30 @@ import {
 } from 'next-share';
 import { TwitterShareButton, TwitterIcon } from 'next-share';
 import { LinkedinShareButton, LinkedinIcon } from 'next-share';
+import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
+import { useTheme } from '@/context/ThemeContext';
+import { cn } from '@/lib/utils';
+import { useCopyToClipboard } from '@uidotdev/usehooks';
+import { useState } from 'react';
 
 interface FunctionProps {
 	blog: BlogMeta;
 }
 
 export default function ShareTab({ blog }: FunctionProps) {
+	const { theme } = useTheme();
+	const [hasCopied, copyToClipboard] = useCopyToClipboard();
+	const [copied, setCopied] = useState(() => Boolean(hasCopied));
+
+	function copyBlogURL() {
+		setCopied(false);
+		copyToClipboard(window.location.href);
+		setCopied(true);
+		setTimeout(function () {
+			setCopied(false);
+		}, 1000);
+	}
+
 	return (
 		<div className="mt-8 mb-2">
 			<span className="flex items-center gap-x-1 font-mono text-xs uppercase">
@@ -51,6 +69,23 @@ export default function ShareTab({ blog }: FunctionProps) {
 				>
 					<LinkedinIcon size={32} round />
 				</LinkedinShareButton>
+
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<button
+							className={cn(
+								'ease rounded-full p-2 transition hover:cursor-pointer',
+								theme === 'dark' ? 'bg-white text-black' : 'bg-black text-white'
+							)}
+							onClick={copyBlogURL}
+						>
+							{copied ? <Check size={16} /> : <Link size={16} />}
+						</button>
+					</TooltipTrigger>
+					<TooltipContent>
+						<p>{copied ? 'Copied!' : 'Copy link'}</p>
+					</TooltipContent>
+				</Tooltip>
 			</div>
 		</div>
 	);
