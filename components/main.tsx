1311b8c049f43c023c2c47cdbec351c683f70d10
--global <sunil.shastry69@gmail.com>
2025-10-31 14:18:04 -0600

    Update selection background colour based on theme (opacity difference)

--- components/main.tsx
+++ components/main.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useTheme } from '@/context/ThemeContext';
+import { cn } from '@/lib/utils';
 
 interface FunctionProps {
 	children: Readonly<React.ReactNode>;
@@ -10,7 +11,12 @@ export default function Main({ children }: FunctionProps) {
 	return (
 		<>
 			<main
-				className={`${theme === 'dark' ? 'bg-black text-gray-300 selection:bg-white selection:text-black' : 'bg-white text-gray-600 selection:bg-black selection:text-white'} min-h-screen pr-8 pl-12 transition max-lg:pr-5 max-lg:pl-8`}
+				className={cn(
+					theme === 'dark'
+						? 'bg-black text-gray-300 selection:bg-[#E5FFC3] selection:text-black'
+						: 'bg-white text-gray-600 selection:bg-[#E5FFC3]/80',
+					'min-h-screen pr-8 pl-12 transition max-lg:pr-5 max-lg:pl-8'
+				)}
 			>
 				{children}
 			</main>
