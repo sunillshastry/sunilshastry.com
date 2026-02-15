d630a4f88c0eddd631bdf4b27ace52200867d11f
--global <sunil.shastry69@gmail.com>
2026-02-15 14:17:40 -0600

    Append the ViewAsApi component within the blogs layout.tsx file

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -1,5 +1,6 @@
 import BackTop from '@/components/back-top';
 import BlogHeader from '@/components/blog-header';
+import ViewAsApi from '@/components/view-as-api';
 import type { Metadata } from 'next';
 interface FunctionProps {
 	children: Readonly<React.ReactNode>;
@@ -13,7 +14,8 @@ export default function Layout({ children }: FunctionProps) {
 	return (
 		<section className="relative mx-auto w-1/2 pt-14 pb-8 max-lg:w-[90%]">
 			<BlogHeader />
-			<div className="mt-8">{children}</div>
+			<ViewAsApi />
+			<div className="mt-6">{children}</div>
 			<BackTop />
 		</section>
 	);
