061cd866bb4af55f21df16dd29e0cafebc895fa4
--global <sunil.shastry69@gmail.com>
2026-08-09 18:42:16 -0400

    Create a standalone projects data section to store all the information related to the projects

---
+++ data/projects.ts
@@ -0,0 +1,23 @@
+import { Project } from '@/interfaces/Project';
+
+const projects: Project[] = [
+	{
+		id: 1,
+		name: 'Itihaas',
+		description:
+			'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat necessitatibus debitis ipsa animi consequatur, provident totam pariatur laudantium hic id sint sit accusantium possimus accusamus voluptate nam tempore veniam ea! Asperiores facere repudiandae quam fugit, est, vero voluptatibus blanditiis expedita recusandae in ratione rem dolores praesentium perferendis obcaecati dicta architecto beatae eaque quidem fuga aut. Impedit, maxime aliquid consectetur nemo unde distinctio est nulla sunt eveniet maiores dolorem iure voluptatem totam, doloremque architecto numquam corrupti? Dignissimos, ducimus inventore sint aut doloribus omnis illo alias repellat laborum nulla magni modi facilis delectus optio temporibus adipisci rem vitae dolore, architecto sapiente suscipit!',
+		tags: ['javascript', 'react', 'typescript', 'node.js', 'html'],
+		githubUrl: 'https://github.com/sunillshastry/itihaas-api',
+		liveDemoUrl: 'https://itihaas.netlify.app/',
+	},
+	{
+		id: 2,
+		name: 'DinnerDibs',
+		description:
+			'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat necessitatibus debitis ipsa animi consequatur, provident totam pariatur laudantium hic id sint sit accusantium possimus accusamus voluptate nam tempore veniam ea! Asperiores facere repudiandae quam fugit, est, vero voluptatibus blanditiis expedita recusandae in ratione rem dolores praesentium perferendis obcaecati dicta architecto beatae eaque quidem fuga aut. Impedit, maxime aliquid consectetur nemo unde distinctio est nulla sunt eveniet maiores dolorem iure voluptatem totam, doloremque architecto numquam corrupti? Dignissimos, ducimus inventore sint aut doloribus omnis illo alias repellat laborum nulla magni modi facilis delectus optio temporibus adipisci rem vitae dolore, architecto sapiente suscipit!',
+		tags: ['java', 'spring boot', 'react', 'typescript'],
+		githubUrl: 'http://github.com/dinnerdibs/',
+	},
+];
+
+export default projects;
