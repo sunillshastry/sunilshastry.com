37f5dbbb774a01a2c7900664636ed8d1914f1344
--global <sunil.shastry69@gmail.com>
2026-08-10 15:57:53 -0400

    Update the /projects and /blogs pages to correctly include the BackButton component with attributes

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -1,3 +1,4 @@
+import BackButton from '@/components/back-button';
 import BackTop from '@/components/back-top';
 import BlogHeader from '@/components/blog-header';
 import ViewAsApi from '@/components/view-as-api';
@@ -12,11 +13,21 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: FunctionProps) {
 	return (
-		<section className="relative mx-auto w-1/2 pt-14 pb-8 max-lg:w-[90%]">
-			<BlogHeader />
-			<ViewAsApi />
-			<div className="mt-6">{children}</div>
-			<BackTop />
+		<section className="relative">
+			<BackButton
+				href="/"
+				title="Home"
+				className="absolute top-6 left-0 z-20 max-lg:top-4 max-lg:-left-2"
+			>
+				Home
+			</BackButton>
+
+			<section className="relative mx-auto w-1/2 pt-14 pb-8 max-lg:w-[90%]">
+				<BlogHeader />
+				<ViewAsApi />
+				<div className="mt-6">{children}</div>
+				<BackTop />
+			</section>
 		</section>
 	);
 }
